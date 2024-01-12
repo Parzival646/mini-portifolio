baa65736bc3987b1a52314fb5aac75d09ca93c34
THAYLER PEREIRA DE ALMEIDA <thaylergames123@gmail.com>
2024-01-12 18:16:23 +0000

    Changes to be committed: 	modified:   assets/css/styles.css 	modified:   assets/js/main.js 	modified:   index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,22 @@
 /*=============== FILTERS TABS ===============*/
+const tab = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]'),
 
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tc =>{
+            tc.classList.remove('filters__activate')
+        })
+        target.classList.add('filters__activate')
+
+        tabs.forEach( t =>{
+            t.classList.remove('filter-tab-active')
+        })
+        tab.classList.add('filter-tab-active')
+    })
+})
 
 /*=============== DARK LIGHT THEME ===============*/
 
